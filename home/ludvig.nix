f5882db2cb1465970aaee56df69855ceefe43cf2
1udd3 <ludvig@andrae.se>
2026-08-06 22:02:30 +0200

    git config

--- home/ludvig.nix
+++ home/ludvig.nix
@@ -7,4 +7,15 @@
   home.stateVersion = "26.05";
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+
+    userName = "1udd3";
+    userEmail = "ludvig@andrae.se";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
 }
