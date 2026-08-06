35968dc3a926e5ceb1fc4fbadd94433429481602
1udd3 <ludvig@andrae.se>
2026-08-06 21:22:25 +0200

    Describe change

--- configuration.nix
+++ configuration.nix
@@ -78,13 +78,6 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-	vim
-	git
-	alacritty
-	firefox
- ];
-
 
  nix.settings.experimental-features = [
    "nix-command"
