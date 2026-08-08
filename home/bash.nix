6c00d1536cf9e41ca0d218e46fdf1c4fcdef9a51
1udd3 <ludvig@andrae.se>
2026-08-08 14:33:19 +0200

    bash.nix_added

---
+++ home/bash.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  programs.bash = {
+    enable = true;
+
+    shellAliases = {
+      ll = "ls -lah";
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos-btw";
+      gnr = "~/skripts/gnr.sh"
+    };
+  };
+}
