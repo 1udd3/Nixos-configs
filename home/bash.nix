2268f928b7e15981f9b9c912032a95643e4c873f
1udd3 <ludvig@andrae.se>
2026-08-08 14:52:44 +0200

    fix_gnr

--- home/bash.nix
+++ home/bash.nix
@@ -7,7 +7,30 @@
     shellAliases = {
       ll = "ls -lah";
       rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos-btw";
-      gnr = "~/skripts/gnr.sh";
     };
+  };
+
+
+  home.file.".local/bin/gnr" = {
+	executable = true;
+
+	text = ''
+		#!/usr/bin/env bash
+
+
+		set -e
+
+		sudo nixos-rebuild switch --flake /etc/nixos#nixos-btw
+
+		dir=$(pwd)
+
+		cd /etc/nixos
+
+		git add .
+		git commit -m "$1"
+		git push
+
+		cd "$dir"
+	'';
   };
 }
